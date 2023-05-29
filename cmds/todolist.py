a22adb1ca6da5729cee65ceb3f2852f6460cb95e
janetchung9642 <janetchung96421234@gmail.com>
2023-05-29 22:01:47 +0800

    main

--- cmds/todolist.py
+++ cmds/todolist.py
@@ -7,7 +7,8 @@ class TodoList(Cog_Extension):
     # Initialization 
     def __init__(self, bot):
         self.todo = []
-
+        super().__init__(bot)
+        
         '''
         todo 是一個 list 變數
         你可以在各個function中對self.todo做操作
@@ -22,25 +23,42 @@ class TodoList(Cog_Extension):
     # Add todolist 
     # item 是要增加的待辨事項
     @commands.command()
-    async def AddTodoList(self, ctx, item):
+    async def AddTodoList(self, ctx,item):
+        self.item=item
+        self.todo.append(item)
+        await ctx.send(self.todo)
         pass 
 
     # Remove todolist
     # item 是要移除的待辨事項
     @commands.command()
     async def RemoveTodoList(self, ctx, item):
+        self.item=item
+        try :
+            self.todo.remove(item)
+            await ctx.send(self.todo)
+        except:
+            await ctx.send("此代辦事項不存在")
         pass 
 
     # Sort todolist
     @commands.command()
     async def SortTodoList(self, ctx):
+        self.todo.sort()
+        for item in self.todo:
+            await ctx.send(item)
+        if self.todo==[]:
+            await ctx.send("沒有任何代辦事項")
         pass 
 
 
     # Clear todolist
     @commands.command()
     async def ClearTodoList(self, ctx):
+       self.todo.clear()
+       
        pass 
 
 async def setup(bot):
     await bot.add_cog(TodoList(bot))
+
